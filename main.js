fa0732da154e1ec7ff985c5bf8e80e412e4bd477
Stirling-Silver <113132128+Stirling-Silver@users.noreply.github.com>
2022-12-21 11:23:40 -0700

    Done

--- main.js
+++ main.js
@@ -26,40 +26,16 @@
 
 // let sum = 0;
 
-// for (let i = 5; i <= 50; i++) {
-//   sum += i;
+// for (let term = 5; term <= 50; term++) {
+//   sum += term;
 // }
 
 // console.log(sum);
 
 //Task 5
 
-const canvas = document.getElementById("game-canvas");
-const ctx = canvas.getContext("2d");
-
-let playerX = canvas.width / 2;
-let playerY = canvas.height - 50;
-
-const gameLoop = function () {
-  // Update game state
-
-  // Draw game
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillRect(playerX, playerY, 50, 50);
-
-  requestAnimationFrame(gameLoop);
-};
-
-gameLoop();
-
-document.addEventListener("keydown", function (event) {
-  if (event.key === "ArrowLeft") {
-    playerX -= 10;
-  } else if (event.key === "ArrowRight") {
-    playerX += 10;
-  } else if (event.key === "ArrowUp") {
-    playerY -= 10;
-  } else {
-    playerY += 10;
-  }
-});
+let sum = 0;
+for (let term = 10; term <= 100; term += 10) {
+  sum += term;
+}
+console.log(sum); // 550
