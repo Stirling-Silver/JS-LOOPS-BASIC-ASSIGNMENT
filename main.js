1504f067f54d18a96785dc6c4a10db30a17cac93
Stirling-Silver <113132128+Stirling-Silver@users.noreply.github.com>
2022-12-22 10:50:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -34,8 +34,37 @@
 
 //Task 5
 
+// let sum = 0;
+// for (let term = 10; term <= 100; term += 10) {
+//   sum += term;
+// }
+// console.log(sum); // 550
+
+//make a thing that counts all the multiples of 8 from 0 to 100
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 8 === 0) {
+//     console.log(i);
+//   }
+// }
+
+//I enjoy my life
+
+// for (let i = 0; i < 1000; i++) {
+//   console.log("I enjoy my life!");
+// }
+
+//Count down even/ then odd numbers from 123
+
+// for (let i = 123; i >= 21; i--) {
+//   if (i % 2 !== 0) console.log(i);
+// }
+
+// !== means its not equal to
+
 let sum = 0;
-for (let term = 10; term <= 100; term += 10) {
+
+for (let term = 0; term <= 120; term += 12) {
   sum += term;
 }
-console.log(sum); // 550
+console.log(sum);
